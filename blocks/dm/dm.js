455deb74a2455763504808c4b28599bcd84b957f
Chris Pilsworth <chrisp@adobe.com>
2025-03-26 12:34:24 +0000

    dm iframe size

--- blocks/dm/dm.js
+++ blocks/dm/dm.js
@@ -5,13 +5,11 @@ export default function decorate(block) {
     try {
         let url = new URL(link); // check link is valid
         
-        let iframe = block.createElement('iframe');
+        let iframe = document.createElement('iframe');
         iframe.src = link;
         iframe.width = '100%';
-        iframe.height = '100%';
-        iframe.style = 'border: 0; top: 0; left: 0; width: 100%; height: 100%; position: absolute;';
+        iframe.height = '100%';        
         iframe.allowfullscreen = '';
-        iframe.scrolling = 'no';
         iframe.allow = 'encrypted-media';
         iframe.title = `Content from ${url.hostname}`;
         iframe.loading = 'lazy';
